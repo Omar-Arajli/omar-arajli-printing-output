6f88ff9ddf4db17878f39d20f3417af79422eac5
Omar-Arajli <83961658+Omar-Arajli@users.noreply.github.com>
2021-05-22 15:14:02 -0400

    JS file task-2 added

---
+++ js-assignment-t2.js
@@ -0,0 +1,13 @@
+/*
+Create the expression concatenating variables first_name, last_name, 
+email and string literals needed to complete the sentence reading: 
+"My name is Your-first-name Your-last-name. You can contact me at your-email@mail.com.";
+
+*/
+
+let first_name = "Omar";
+let last_name = "Arajli";
+let email = "arajliomar@gmail.com";
+
+let output= "My name is " + first_name + " " + last_name + ". You can contact me at " + email + ". "; 
+alert(output);

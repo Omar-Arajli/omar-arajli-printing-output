4829262c6a45551c136e52a811b3946b27f56efe
Omar-Arajli <83961658+Omar-Arajli@users.noreply.github.com>
2021-05-22 15:13:19 -0400

    JS file task-1 added

---
+++ js-assignment-t1.js
@@ -0,0 +1,38 @@
+/* 
+Task 1
+
+Declare three variables, a and b and c.
+
+If a has value 3, b has value 5, and c doesn't have a value, alert the following expressions 
+( one after another ). Alert popup box has to show the following content for each expression
+(concatenate everything in one long expression using "\n" to make new lines):
+
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
+
+
+let a = 3;
+let b = 5;
+let c;
+
+
+let outpout = "let a = 3;\nlet b = 5;\nlet c\n----------";
+
+alert (outpout + "\n\na + b = " + (a + b) + "\na - b = " + (a - b) + "\na * b = " + (a * b) + "\na / b = " + (a / b) + "\na % b = " + (a % b) + "\na += b = " + (a += b) + "\na -= b = " + (a -= b) + "\na *= b = " + (a *= b)+ "\na /= b = " + (a /= b)+ "\na %= b = " + (a %= b) + "\na == b = " + (a == b) + "\na != b = " + (a != b) + "\na > b = " + (a > b) + "\na < b = " + (a < b) + "\n!a && !c = " + (!a && !c) + "\n!a || !c  = " + (!a || !c ) + "\n\n" );
+
+
